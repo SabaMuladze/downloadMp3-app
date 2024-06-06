42e1ca5b895199f692a44ecca8715385a20f0951
SabaMuladze <muladze66@bk.ru>
2024-06-06 22:40:00 +0400

    create vite project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <h1>MP3 Downloader</h1>
+    <input v-model="url" placeholder="Enter MP3 URL" />
+    <button @click="downloadMp3">Download</button>
+  </div>
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
